8f04418e6bcdd0bd43c8655d4ecccac4278907bb
BlaiseLegendre <blaise.legendre@gmail.com>
2019-10-06 16:17:10 -0400

    Add existing files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("scss/*.scss", gulp.series('sass'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("pages/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.series('serve'));
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<title>I thought what I'd do was, I'd pretend I was one of those deaf-mutes</title>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="css/mixins.css">
+</head>
+
+<body>
+<article class="landing_page">
+	<section class="landing_page__menu">
+		 <?php include('sections/menu.html'); ?>
+		 
+	</section> 	 	
+</article>
+</body>
+</html>
